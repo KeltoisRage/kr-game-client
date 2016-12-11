ae09955e79964cf462a2cc495f7de7ead7045ada
Antonio Cao <antoniocao000@gmail.com>
2016-12-11 22:36:49 +0100

    Fuchikada Update

--- js/xylose/Component.js
+++
@@ -1,9 +0,0 @@
-Xylose.Component = function (game) {
-	this.game = game;
-};
-
-Xylose.Component.prototype = {
-	loadContent: function (delta) {},
-	update: function (delta) {},
-	draw: function (delta) {}
-};
